3aa09d777c585c6ea75b2c81eb56173bbce9ac9f
Moritz Schäfer <moritz.schaefer@pm.me>
2023-02-06 19:32:02 +0100

    added bias __init__.py

---
+++ erbs/bias/__init__.py
@@ -0,0 +1,4 @@
+from erbs.bias.potential import GKernelMTD
+from erbs.bias.energy_function_factory import energy_fn_factory, opes_energy_fn_factory
+
+__all__ = ["GKernelMTD", "energy_fn_factory", "opes_energy_fn_factory"]
