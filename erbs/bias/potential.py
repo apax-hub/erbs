216f99ede0c1576bf01bd57e8f616852b3317f2b
Moritz Schaefer <moritz.schaefer@pm.me>
2023-03-14 13:43:08 +0100

    logging sketch

--- erbs/bias/potential.py
+++ erbs/bias/potential.py
@@ -1,9 +1,16 @@
+from pathlib import Path
 import jax
 import jax.numpy as jnp
 import numpy as np
 from ase.calculators.calculator import Calculator, all_changes
+import h5py
 
 from erbs.cv.cv_nl import compute_cv_nl
+
+def append_to_ds(ds, new_data):
+    shape = ds.shape[0] + new_data.shape[0]
+    ds.resize(shape, axis=0)
+    ds[-new_data.shape[0]:,...] = new_data
 
 
 class GKernelMTD(Calculator):
@@ -11,6 +18,7 @@ class GKernelMTD(Calculator):
 
     def __init__(
         self,
+        base_calc,
         cv_fn,
         dim_reduction_factory,
         energy_fn_factory,
@@ -18,9 +26,18 @@ class GKernelMTD(Calculator):
         k,
         a,
         interval=100,
+        log_file="labels.hdf5",
         **kwargs
     ):
         Calculator.__init__(self, **kwargs)
+
+        if not isinstance(base_calc, Calculator):
+            raise ValueError(
+                "All the calculators should be inherited from" \
+                "the ase's Calculator class"
+            )
+        self.base_calc = base_calc
+        self.log_file = log_file
 
         self.k = k
         self.a = a
@@ -41,6 +58,30 @@ class GKernelMTD(Calculator):
         self.interval = interval
         self._step_counter = 0
         self.accumulate = True
+        # self.data_h5 = None
+        self.data_h5 = h5py.File(self.log_file, "w", libver='latest')
+        self.initialized = False
+
+    def _initialize_g_ds(self):
+        g_grp = self.data_h5.create_group("descriptors")
+        cv_shape = self.ref_cvs[0].shape#[1] + list(self.ref_cvs[0].shape)
+        g_grp.create_dataset("full", data=self.ref_cvs[0][::-1], maxshape=(None, cv_shape[1]), dtype=np.float32)
+        g_grp.create_dataset("atomic_numbers", data=self.ref_atomic_numbers[0][::-1], maxshape=(None, ), dtype=np.int32)
+
+
+    def _initialize_label_ds(self):
+
+        label_grp = self.data_h5.create_group("labels")
+        E_base = self.energy_buffer#np.array([self.base_results["energy"]])
+        E_bias = self.energy_bias_buffer#np.array([self.bias_results["energy"]])
+        
+        label_grp.create_dataset("energy", data=E_base, maxshape=(None,), dtype=np.float64)
+        label_grp.create_dataset("energy_bias", data=E_bias, maxshape=(None,), dtype=np.float64)
+
+        n_atoms = self.base_results["forces"].shape[0]
+        label_grp.create_dataset("forces", data=self.forces_buffer, maxshape=(None,n_atoms,3), dtype=np.float64)
+        label_grp.create_dataset("forces_bias", data=self.forces_bias_buffer, maxshape=(None,n_atoms,3), dtype=np.float64)
+
 
     def update_bias(self, atoms):
         position = jnp.array(atoms.positions, dtype=jnp.float32)
@@ -79,37 +120,95 @@ class GKernelMTD(Calculator):
             neighbor = neighbor.update(positions)
             energy, neg_forces = jax.value_and_grad(energy_fn)(positions, neighbor)
             forces = -neg_forces
-            return energy, forces, neighbor
+
+            return {"energy": energy, "forces": forces}, neighbor
 
         self.energy_and_force_fn = body_fn
+
+
+    def dump_data(self):
+        append_to_ds(self.data_h5["descriptors/full"], self.ref_cvs[-1])
+        append_to_ds(self.data_h5["descriptors/atomic_numbers"], self.ref_atomic_numbers[-1])
+
+        append_to_ds(self.data_h5["labels/energy"], self.energy_buffer)
+        append_to_ds(self.data_h5["labels/forces"], self.forces_buffer)
+
+        append_to_ds(self.data_h5["labels/energy_bias"], self.energy_bias_buffer)
+        append_to_ds(self.data_h5["labels/forces_bias"], self.forces_bias_buffer)
+
+    # def dump_g(self):
+    #     append_to_ds(self.data_h5["descriptors/full"], self.ref_cvs[-1])
+    #     append_to_ds(self.data_h5["descriptors/atomic_numbers"], self.ref_atomic_numbers[-1])
+
+    # def dump_labels(self):
+    #     append_to_ds(self.data_h5["labels/energy"], self.energy_buffer)
+    #     append_to_ds(self.data_h5["labels/forces"], self.forces_buffer)
+
+    #     append_to_ds(self.data_h5["labels/energy_bias"], self.energy_bias_buffer)
+    #     append_to_ds(self.data_h5["labels/forces_bias"], self.forces_bias_buffer)
+        
+    def reset_buffer(self, n_atoms):
+        self.energy_buffer = np.zeros(self.interval)
+        self.forces_buffer = np.zeros((self.interval, n_atoms, 3))
+        self.energy_bias_buffer = np.zeros(self.interval)
+        self.forces_bias_buffer = np.zeros((self.interval, n_atoms, 3))
+
+    def write_to_buffer(self):
+        i = self._step_counter % self.interval
+        print(self._step_counter, i)
+        self.energy_buffer[i] = self.base_results["energy"]
+        self.forces_buffer[i] = self.base_results["forces"]
+        self.energy_bias_buffer[i] = self.bias_results["energy"]
+        self.forces_bias_buffer[i] = self.bias_results["forces"]
 
     def calculate(self, atoms=None, properties=["energy"], system_changes=all_changes):
         Calculator.calculate(self, atoms, properties, system_changes)
 
-        update_bias = self._step_counter % self.interval == 0
-        if update_bias and self.accumulate:
+        self.base_calc.calculate(atoms, properties, system_changes)
+        self.base_results = self.base_calc.results
+
+        should_update_bias = self._step_counter % self.interval == 0
+        if should_update_bias and self.accumulate:
             self.update_bias(atoms)
 
+        if self._step_counter == 0:
+            self._initialize_g_ds()
+            self.reset_buffer(len(atoms))
+
         position = jnp.array(atoms.positions, dtype=jnp.float32)
-        energy, forces, self.neighbors = self.energy_and_force_fn(
+        bias_results, self.neighbors = self.energy_and_force_fn(
             position, self.neighbors
         )
 
         if self.neighbors.did_buffer_overflow:
             print("neighbor list overflowed, reallocating.")
             self.neighbors = self.neighbor_fn.allocate(position)
-            energy, forces, self.neighbors = self.energy_and_force_fn(
+            bias_results, self.neighbors = self.energy_and_force_fn(
                 position, self.neighbors
             )
+        self.bias_results = {k: np.array(v, dtype=np.float64) for k,v in bias_results.items()}
+        
+        # print(self.base_results["energy"], self.bias_results["energy"])
+        print(self.base_results["energy"])
+        if should_update_bias:
+            self.reset_buffer(len(atoms))
+        self.write_to_buffer()
+
+        print(self.energy_buffer)
+        should_dump = self._step_counter % self.interval == 1
+        if should_dump:
+            # print(self._step_counter)
+            if self._step_counter >= self.interval and not self.initialized:
+                self._initialize_label_ds()
+                self.initialized = True
+            elif self.initialized:
+                self.dump_data()
 
         self.results = {
-            "energy": np.array(energy, dtype=np.float64),
-            "forces": np.array(forces, dtype=np.float64),
+            "energy": self.base_results["energy"], #+ self.bias_results["energy"],
+            "forces": self.base_results["forces"], #+ self.bias_results["forces"],
         }
         self._step_counter += 1
-
-    def save_descriptors(self, path):
-        np.savez(path, g=self.ref_cvs, z=self.ref_atomic_numbers)
 
     def add_configs(self, atoms_list):
         @jax.jit
